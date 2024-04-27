b91b1312e4acd12f188733991075a1f107c72c6c
GustavoM4tias <128002171+GustavoM4tias@users.noreply.github.com>
2024-04-26 21:47:53 -0300

    responsive about update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,10 +2,10 @@
   <div class="container-fluid">
     <div class="row justify-content-center">
       <div
-        class="card card-personagens col-lg-8 col-sm-10 m-5 justify-content-center align-items-center"
+        class="card card-personagens col-lg-8 col-md-10 col-sm-10 m-5 justify-content-center align-items-center"
       >
         <div class="row">
-          <div class="col-md-6">
+          <div class="col-md-6 col-sm-12">
             <img
               class="card-img imagem"
               src="../assets/img/gustavodiniz.png"
@@ -14,12 +14,8 @@
           </div>
           <div class="col-md-6 col-sm-12">
             <div class="card-body">
-              <p class="card-text nome">
-                <strong>Nome:</strong> Gustavo "Smith" Diniz 
-              </p>
-              <p class="card-text">
-                <strong>Status:</strong> Alive (ainda bem)
-              </p>
+              <p class="card-text nome"><strong>Nome:</strong> Gustavo Diniz</p>
+              <p class="card-text"><strong>Status:</strong> Alive</p>
               <p class="card-text"><strong>Espécie:</strong> Human</p>
               <p class="card-text"><strong>Gênero:</strong> Male</p>
               <p class="card-text"><strong>Local:</strong> Earth</p>
@@ -30,8 +26,15 @@
                 Aparece em <strong>algumas</strong> aulas.
               </p>
               <div class="botoes">
-                <a href="https://github.com/GustavoM4tias/rickapi" target="_blank" class="btn btn-dark m-2"><i class="bi bi-github"></i> Github</a>
-                <a href="#" target="_blank" class="btn btn-primary"><i class="bi bi-linkedin"></i> LinkedIn</a>
+                <a
+                  href="https://github.com/GustavoM4tias/rickapi"
+                  target="_blank"
+                  class="btn btn-dark m-2"
+                  ><i class="bi bi-github"></i> Github</a
+                >
+                <a href="#" target="_blank" class="btn btn-primary"
+                  ><i class="bi bi-linkedin"></i> LinkedIn</a
+                >
               </div>
             </div>
           </div>
@@ -40,3 +43,49 @@
     </div>
   </div>
 </template>
+
+<style>
+.nome {
+  margin-top: 30px;
+}
+
+p {
+  margin: 0 0 5px 10px;
+}
+
+.imagem {
+  object-fit: cover;
+  border-radius: 15px;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.1);
+  height: 60vh;
+  transform: scaleX(-1);
+}
+
+.card-personagens {
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.1);
+  transition: 0.3s;
+  height: 70vh;
+}
+
+.card-personagens:hover {
+  transform: scale(1.02);
+  transition: 0.3s;
+}
+
+@media only screen and (max-width: 600px) {
+  .imagem {
+    margin-top: 15px;
+    object-fit: cover;
+    border-radius: 15px;
+    box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.1);
+    height: auto;
+    transform: scaleX(-1);
+  }
+
+  .card-personagens {
+    box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.1);
+    transition: 0.3s;
+    height: auto;
+  }
+}
+</style>
